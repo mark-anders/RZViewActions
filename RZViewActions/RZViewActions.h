1f6ec4ff9750f725cc2826c5bc5ac480b1a59c1f
Mark Anders <mark@markanders.net>
2015-12-18 21:51:19 -0700

    Forgot to make the schemes share, as required by Carthage

---
+++ RZViewActions/RZViewActions.h
@@ -0,0 +1,21 @@
+//
+//  RZViewActions.h
+//  RZViewActions
+//
+//  Created by Mark Anders on 12/18/15.
+//  Copyright © 2015 Raizlabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RZViewActions.
+FOUNDATION_EXPORT double RZViewActionsVersionNumber;
+
+//! Project version string for RZViewActions.
+FOUNDATION_EXPORT const unsigned char RZViewActionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RZViewActions/PublicHeader.h>
+#import <RZViewActions/UIView+RZViewActions.h>
+
+
+
